571cdfdee6ff25140f2f2dd5b494dec982fbdd62
Alyssa <asilla7@gmail.com>
2025-03-05 18:13:00 -0800

    Updates

---
+++ precache-manifest.78b76c4cd4212e5ff0d1a4533431de02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db4fecc8d9e878547437a1658b31d8d",
+    "url": "/cocina-dashboard/index.html"
+  },
+  {
+    "revision": "7224a2f31b70fdee27bf",
+    "url": "/cocina-dashboard/static/css/2.05cbb0d5.chunk.css"
+  },
+  {
+    "revision": "abe8dd64726a5a419cc2",
+    "url": "/cocina-dashboard/static/css/main.0acd0656.chunk.css"
+  },
+  {
+    "revision": "7224a2f31b70fdee27bf",
+    "url": "/cocina-dashboard/static/js/2.f9823929.chunk.js"
+  },
+  {
+    "revision": "1179fe98c15d1aaf04d6acdd8c0e4128",
+    "url": "/cocina-dashboard/static/js/2.f9823929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd95160b3791e14a8ec",
+    "url": "/cocina-dashboard/static/js/3.4abda6f6.chunk.js"
+  },
+  {
+    "revision": "abe8dd64726a5a419cc2",
+    "url": "/cocina-dashboard/static/js/main.6bb0253f.chunk.js"
+  },
+  {
+    "revision": "deaffe82d181d9dbf022",
+    "url": "/cocina-dashboard/static/js/runtime-main.7dbd93af.js"
+  }
+]);
